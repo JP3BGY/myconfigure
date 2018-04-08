8e36ca3f3f7892d76bd8a1489fb9abe9ad971db0
jp3bgy <heb52204@hcc5.bai.ne.jp>
2018-04-09 00:24:25 +0900

    change template

--- linux/.vim/template/pro.cpp
+++ linux/.vim/template/pro.cpp
@@ -2,13 +2,21 @@
 
 #define FOR(i, a, b) for(int i = a;i < b;i++)
 #define For(i, a) FOR(i, 0, a)
-#define REV(i, a, b) for(int i = b-1;i >= a;i++)
+#define REV(i, a, b) for(int i = b-1;i >= a;i--)
 #define Rev(i, a) REV(i, 0, a)
 #define REP(a) For(i, a)
+#ifdef DEBUG
+#define DEBUG_PRINT(...) printf(__VA_ARGS__)
+#else
+#define DEBUG_PRINT(...)
+#endif
 
 using namespace std;
 typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+
 
 int main(){
-    return 0;
+  return 0;
 }
