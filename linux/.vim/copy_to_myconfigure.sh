10d5e1f272d8b4324ebe2c3b31ca0f031381e50d
jp3bgy <jp3bgy@localhost.localdomain>
2018-08-24 20:59:50 +0900

    add sh file for update

---
+++ linux/.vim/copy_to_myconfigure.sh
@@ -0,0 +1,5 @@
+cp rc/ ~/myconfigure/linux/.vim -r
+cp ftplugin/ ~/myconfigure/linux/.vim -r
+cp neosnippets/ ~/myconfigure/linux/.vim -r
+cp template/ ~/myconfigure/linux/.vim -r
+cp vimrc ~/myconfigure/linux/.vim -r
