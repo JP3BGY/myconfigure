cdcc443f3ad330a5559f89a0e29d826768160895
JP3BGY <heb52204@hcc5.bai.ne.jp>
2018-09-22 00:25:50 +0900

    modify copytouser.sh

--- copytouser.sh
+++ copytouser.sh
@@ -4,9 +4,9 @@ cp -r linux/.gvimrc ~
 cp -r linux/.vim/ ~
 cp -r linux/.zlogout ~
 cp -r linux/.zshrc ~
-if [! -e ~/.vim/repos/github.com/Shougo];then
+if [ ! -e ~/.vim/repos/github.com/Shougo ] ; then
     mkdir -p ~/.vim/repos/github.com/Shougo
 fi
-if [! -e ~/.vim/repos/github.com/Shougo/dein.vim];then
+if [ ! -e ~/.vim/repos/github.com/Shougo/dein.vim ] ; then
     git clone https://github.com/Shougo/dein.vim.git ~/.vim/repos/github.com/Shougo/dein.vim
 fi
